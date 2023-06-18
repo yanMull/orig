b380e9edd4c19c3ca9268e98d6470685e081b049
Yan <pelpvsp@gmail.com>
2023-06-18 18:29:47 +0300

    z19

--- sem2/Program.cs
+++ sem2/Program.cs
@@ -1,10 +1,9 @@
 ﻿Console.Clear();
-int num = new Random().Next(100,1000);
+int num = new Random().Next(100,10000);
 Console.WriteLine(num);
 int num1 = num / 100;
 int num2 = num % 10;
 int num3 = num1 * 10 + num2;
 Console.WriteLine(num3);
 // короткий прикольный вариант вместо верхнего
-Console.WriteLine($"{num} -> {num/100%100}");
-
+Console.WriteLine($"{num} -> {num/100%10}");
---
+++ sem3dz/Z19/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine ("enter num: ");
+int n = int.Parse(Console.ReadLine()!);
+int a = n%10;
+int b = n%100/10;
+int c = n/10000;
+int d = n/1000%10;
+if (a == c) {
+    if (b == d) Console.WriteLine ("yes");
+} 
+else Console.WriteLine ("no");
