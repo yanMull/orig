dc4534ed7e90a67c7eea65cca967b1099b839b40
Yan <pelpvsp@gmail.com>
2023-06-18 18:34:58 +0300

    z21

---
+++ sem3dz/z21/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Enter num");
+int n = int.Parse(Console.ReadLine());
+int i = 1;
+if (n<i)
+{Console.WriteLine("Error n");
+return;
+}
+while(i<=n)
+{
+    Console.Write($" {Math.Pow(i,3)}");
+    i++;
+}
+Console.WriteLine("");
