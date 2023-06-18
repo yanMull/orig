e1ae7fb6b192a8bf4dee0dcf378bad1b7dc261e0
Yan <pelpvsp@gmail.com>
2023-06-18 18:31:20 +0300

    z20

---
+++ sem3dz/z20/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine ("введите координаты 1 точки a: ");
+int ax = int.Parse(Console.ReadLine()!);
+int ay = int.Parse(Console.ReadLine()!);
+int az = int.Parse(Console.ReadLine()!);
+Console.WriteLine ("введите координаты 2 точки b: ");
+int bx = int.Parse(Console.ReadLine()!);
+int by = int.Parse(Console.ReadLine()!);
+int bz = int.Parse(Console.ReadLine()!);
+Console.WriteLine ($"расстояние равно {Math.Sqrt(Math.Pow(ax-bx,2)+Math.Pow(ay-by,2)+Math.Pow(az-bz,2)):f2}");
